57534229181f1b80f2fe2d976ee015f80e2a7498
Henrique Almeida <59674959+hicones@users.noreply.github.com>
2025-06-03 14:35:45 -0300

    refactor: upgrade tailwind

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,6 +1,6 @@
 export function HomePage() {
   return (
-    <main className="flex items-center justify-center">
+    <main className="flex items-center justify-center h-lvh">
       <h1>Vite + React</h1>
     </main>
   );
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 import { defineConfig } from "vite";
 
 const APP_PORT = process.env.NODE_ENV === "development" ? 5173 : 80;
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
 
   preview: {
     host: true,
